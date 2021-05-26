f5ca25515bba10c38efa6cbd6b892dbba58e1cd4
Viatcheslav_Suhodoiev <suhodoiev@gmail.com>
2021-05-26 20:18:43 +0300

    Stage 3/5 completed

--- Food Blog Backend/task/blog.py
+++ Food Blog Backend/task/blog.py
@@ -3,70 +3,77 @@ import sqlite3
 database = r'C:\Python\Projects\Food Blog Backend\Food Blog Backend\task\food_blog.db'
 
 conn = sqlite3.connect(database)
-cursor_1 = conn.cursor()
+c = conn.cursor()
 
-result = cursor_1.execute('''CREATE TABLE meals (meal_id integer PRIMARY KEY, 
+result = c.execute('''CREATE TABLE if not exists meals (meal_id integer PRIMARY KEY, 
                                         meal_name VARCHAR (20) NOT NULL UNIQUE);''')
 
-result_2 = cursor_1.execute('''CREATE TABLE ingredients (ingredient_id integer PRIMARY KEY, 
+c.execute('''DELETE FROM meals''')
+c.execute('''CREATE TABLE if not exists ingredients (ingredient_id integer PRIMARY KEY, 
                                                          ingredient_name VARCHAR (20) NOT NULL UNIQUE);''')
-
-result_3 = cursor_1.execute('''CREATE TABLE measures (measure_id integer PRIMARY KEY, 
+c.execute('''DELETE FROM ingredients''')
+c.execute('''CREATE TABLE if not exists measures (measure_id integer PRIMARY KEY, 
                                                       measure_name VARCHAR (40) UNIQUE);''')
 
-result_4 = cursor_1.execute('''INSERT INTO meals VALUES (1, 'breakfast'), 
-                                                        (2, 'brunch'),
-                                                        (3, 'lunch'),
-                                                        (4, 'supper')''')
+c.execute('''DELETE FROM measures''')
+c.execute('''CREATE TABLE  if not exists recipes(recipe_id int PRIMARY KEY,
+                                                 recipe_name VARCHAR(20) NOT NULL,
+                                                 recipe_description VARCHAR(50));''')
+c.execute('''DELETE FROM recipes''')
 
+c.execute('''PRAGMA foreign_keys = ON;''')
 
-result_5 = cursor_1.execute('''INSERT INTO ingredients VALUES   (1, 'milk'), 
-                                                                (2, 'cacao'),
-                                                                (3, 'strawberry'),
-                                                                (4, 'blueberry'),
-                                                                (5, 'blackberry'),
-                                                                (6, 'sugar')''')
+c.execute('''CREATE TABLE if not exists serve(
+             serve_id INTEGER PRIMARY KEY,
+             recipe_id INTEGER NOT NULL,
+             meal_id INTEGER NOT NULL,
+             FOREIGN KEY(recipe_id) REFERENCES recipes(recipe_id),
+             FOREIGN KEY (meal_id) REFERENCES meals(meal_id)
+             )''')
 
-
-result_6 = cursor_1.execute('''INSERT INTO measures (measure_id, measure_name) VALUES  (1,'ml'), 
-                                                            (2,'g'),
-                                                            (3,'l'),
-                                                            (4,'cup'),
-                                                            (5,'tbsp'),
-                                                            (6,'tsp'),
-                                                            (7,'dsp'),
-                                                            (8,'')''')
 conn.commit()
 
-result_7 = cursor_1.execute('''CREATE TABLE recipes(recipe_id int PRIMARY KEY,
-                                                    recipe_name VARCHAR(20) NOT NULL,
-                                                    recipe_description VARCHAR(50));''')
 
-ask_on = True
+data = {"meals": ("breakfast", "brunch", "lunch", "supper"),
+        "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
+        "measures": ("ml", "g", "l", "cup", "tbsp", "tsp", "dsp", "")}
+
+
+for table in data:
+    for row in data[table]:
+        c.execute(f"""INSERT INTO {table} ({table[:-1]}_name) Values ('{row}')""")
+
+conn.commit()
+
+
+print('Pass the empty recipe name to exit.')
+
 n = 0
-while ask_on:
-    recipe_n = input('What is meal name?')
-    if not recipe_n:
-        ask_on = False
-        continue
-    cooking_directions = input('What is cooking directions?')
-    sql = '''INSERT INTO recipes VALUES (?,?,?)'''
-    n += 1
-    result_8 = cursor_1.execute(sql, (n, recipe_n, cooking_directions))
+while True:
+    name = input('Meal name?')
+    if name == '':
+        break
+    else:
+        description = input('Recipe description')
+        new_recipe_id = c.execute(f"""INSERT INTO recipes (recipe_name, recipe_description)
+                                      VALUES ('{name}', '{description}')
+                                   """).lastrowid
+        conn.commit()
 
+        c.execute(f"""SELECT meal_id, meal_name FROM meals
+                        """)
+        meals = c.fetchall()
+        conn.commit()
 
-result = cursor_1.execute('''
-                            PRAGMA foreign_keys = ON;
-                            ''')
+        for meal in meals:
+            print(f"{meal[0]}) {meal[1]}", end=" ") # print('1) breakfast  2) brunch  3) lunch  4) supper')
 
+        when = input("\nWhen the dish can be served: ").split()
 
-result_1 = cursor_1.execute('''CREATE TABLE IF NOT EXISTS  serve (
-                                                 serve_id int PRIMARY KEY,
-                                                 recipe_id int NOT NULL,
-                                                 meal_id int NOT NULL
-                                                 );''')
+        for w in when:
+            c.execute(f"""INSERT INTO serve (recipe_id, meal_id)
+                               VALUES ('{new_recipe_id}', '{w}')
+                               """)
+        conn.commit()
 
-
-
-conn.commit()
 conn.close()
